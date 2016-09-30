ceba18a189398b2966648dcfbbf9659ec6e4b686
Kevin OBrien <kevin.obrien@praece.com>
2016-09-30 14:16:02 -0700

    Testing install scripts

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Update with your config settings.
 var fs = require('fs');
+var path = require('path');
 
 // if (fs.existsSync('./config/local.js')) {
 //   var local = require('./config/local.js').connections.postgresql;
@@ -8,7 +9,9 @@ var fs = require('fs');
 //   var local = {};
 // }
 
-console.log(`----------happening!--------\n`);
+console.log(`----------happening!--------\n`, __dirname);
+console.log(`----------happening!--------\n`, path.resolve(__dirname));
+console.log(`----------happening!--------\n`, path.resolve('../'));
 
 module.exports = {
 
